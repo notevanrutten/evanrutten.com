ef4368d26ed217c16650c46d27bf726d5a9f2c81
Evan Rutten <contact@evanrutten.com>
2023-08-29 20:44:09 -0400

    Add auto-updating copyright year

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
 
+    eleventyConfig.addShortcode("year", () => {
+        return new Date().getFullYear();
+    });
+
     return {
         dir: {
             input: "src",
