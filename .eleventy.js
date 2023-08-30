bcdd082eadc094a1f46d35602e08964c9a142ceb
Evan Rutten <contact@evanrutten.com>
2023-08-29 22:35:12 -0400

    Restructure file system

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({ "./src/images": "/" });
+    eleventyConfig.addPassthroughCopy({ "./src/assets": "/" });
 
     eleventyConfig.addShortcode("year", () => {
         return new Date().getFullYear();
