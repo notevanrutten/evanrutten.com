44236a5e44947489040b42cc2cccb33347a167e9
Evan Rutten <contact@evanrutten.com>
2023-09-19 13:59:17 -0400

    Fix image loading

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({ "./src/assets": "/" });
-    eleventyConfig.addWatchTarget("./src/sass/");
+    eleventyConfig.addPassthroughCopy({ "src/assets/": "/assets" });
+    eleventyConfig.addWatchTarget("src/sass/");
 
     eleventyConfig.addShortcode("year", () => {
         return new Date().getFullYear();
